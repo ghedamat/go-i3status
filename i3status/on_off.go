b6fa287dcc5267adbc0fa458df0d4d573fa1b79b
Mattia Gheda <ghedamat@gmail.com>
2014-01-08 22:06:02 -0500

    pants on, pants off

--- i3status/on_off.go
+++ i3status/on_off.go
@@ -3,12 +3,12 @@ package i3status
 import (
 	"fmt"
 	"strconv"
-	"time"
 )
 
 type OnOffWidget struct {
 	BaseWidget
 	Input chan Entry
+	On    bool
 }
 
 func NewOnOffWidget(output chan Message, input chan Entry) *OnOffWidget {
@@ -20,21 +20,39 @@ func NewOnOffWidget(output chan Message, input chan Entry) *OnOffWidget {
 			instanceCount,
 		},
 		input,
+		false,
 	}
 	return &w
 }
 
-func (w *OnOffWidget) basicLoop() {
-	msg := NewMessage()
-	msg.Name = "Date"
-	msg.Color = "#ffffff"
-	msg.Instance = strconv.Itoa(w.Instance)
-	for {
-		msg.FullText = fmt.Sprintf("%s", time.Now())
-		w.Output <- *msg
-		time.Sleep(w.Refresh * time.Millisecond)
+func (w *OnOffWidget) message() string {
+	if w.On {
+		return "Pants On"
+	} else {
+		return "Pants Off"
 	}
 }
+
+func (w *OnOffWidget) sendMessage() {
+	msg := NewMessage()
+	msg.Name = "OnOff"
+	msg.Color = "#ffffff"
+	msg.Instance = strconv.Itoa(w.Instance)
+	msg.FullText = fmt.Sprint(w.message())
+	w.Output <- *msg
+}
+
+func (w *OnOffWidget) readLoop() {
+	<-w.Input
+	if w.On {
+		w.On = false
+	} else {
+		w.On = true
+	}
+	go w.sendMessage()
+}
+
 func (w *OnOffWidget) Start() {
-	go w.basicLoop()
+	go w.sendMessage()
+	go w.readLoop()
 }
--- test/on_off_test.go
+++ test/on_off_test.go
@@ -19,3 +19,33 @@ func TestOnOffWidgetConstructor(t *testing.T) {
 		})
 	})
 }
+
+func TestOnOffWidget(t *testing.T) {
+	Convey("Given an OnOff Widget", t, func() {
+		c := make(chan i3status.Message)
+		i := make(chan i3status.Entry)
+		w := i3status.NewOnOffWidget(c, i)
+		w.Start()
+		Convey("When and entry is sent", func() {
+			i <- i3status.Entry{}
+			Convey("widget status goes on", func() {
+				So(w.On, ShouldEqual, true)
+			})
+		})
+	})
+	Convey("Given an OnOff Widget", t, func() {
+		c := make(chan i3status.Message)
+		i := make(chan i3status.Entry)
+		w := i3status.NewOnOffWidget(c, i)
+		w.Start()
+		Convey("When and entry is sent", func() {
+			msg := <-c
+			So(msg.FullText, ShouldEqual, "Pants Off")
+			i <- i3status.Entry{}
+			Convey("widget message is On", func() {
+				msg := <-c
+				So(msg.FullText, ShouldEqual, "Pants On")
+			})
+		})
+	})
+}
